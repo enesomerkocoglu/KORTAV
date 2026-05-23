eeee938ac033fef8f7f14e54ba5d9918d553b5ef
Enes Ömer Koçoğlu <enesomerk1@gmail.com>
2026-05-23 11:12:04 +0300

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CartProvider } from './CartContext'
 import App from './App'
 import './styles.css'
 
-import { HashRouter } from 'react-router-dom'
-
-<HashRouter>
-  <CartProvider>
-    <App />
-  </CartProvider>
-</HashRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HashRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </HashRouter>
+  </StrictMode>
+)
